105853ac77237260e09dc0ed00da2e8cc9c1d150
al9ol <34196533+al9ol@users.noreply.github.com>
2017-12-09 15:13:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ def train_net(cifar10_dir=r'./datasets/cifar-10-batches-py',
                 manager.create_model(cnn.ConvReLUPoolDropAffineSoftmax,
                                      X_train.shape[1:], **params)
 
-                manager.train(X_train[:10], y_train[:10], X_val, y_val, X_test, y_test,
+                manager.train(X_train, y_train, X_val, y_val, X_test, y_test,
                               n_epoch=1, batch_size=64, verbose=True,
                               save_model_to_dir=model_dir,
                               save_model_every_epoch=10,
